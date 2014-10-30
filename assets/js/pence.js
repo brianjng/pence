6c843a05f89104febd04f809e53758f8868ef08a
Josef Kelly <kelly.josef@gmail.com>
2014-10-29 17:42:11 -0700

    bump

--- assets/js/pence.js
+++ assets/js/pence.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 	$('img').addClass('img-responsive')
 
 	$('.tags a').each(function() {
-		$(this).addClass('btn btn-small btn-default')
+		$(this).addClass('btn btn-sm btn-default')
 			.prepend('<i class="fa fa-tag"></i> ')
 	})
 })
