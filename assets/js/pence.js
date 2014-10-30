f52958204eb68e2d69599c616faa0b282dd5fb9a
Josef Kelly <kelly.josef@gmail.com>
2014-10-29 17:28:17 -0700

    a little tag js magic

--- assets/js/pence.js
+++ assets/js/pence.js
@@ -1,3 +1,8 @@
 $( document ).ready(function() {
 	$('img').addClass('img-responsive')
+
+	$('.tags a').each(function() {
+		$(this).addClass('btn btn-small btn-default')
+			.prepend('<i class="fa fa-tag"></i>')
+	})
 })
