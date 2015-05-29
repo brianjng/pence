0cf1e72873b10a85c2b71435f61a2f586d4f008e
Josef Kelly <sith+gh@josef.io>
2015-05-28 17:48:41 -0700

    minor stuff

--- assets/js/vinyl.js
+++ assets/js/vinyl.js
@@ -1,11 +1,6 @@
 $( document ).ready(function() {
-	$('img').addClass('img-responsive')
-
-	$('.tags a').each(function() {
-		$(this).addClass('btn btn-sm btn-default')
-			.prepend('<i class="fa fa-tag"></i> ')
-	})
-
-	$('a.older-posts').addClass('pull-right btn btn-default')
-	$('a.newer-posts').addClass('pull-left btn btn-default')
+	$('img').addClass('ui image')
+  $('#navigation-toggle').click(function() {
+    $('#navigation').sidebar('toggle')
+  })
 })
